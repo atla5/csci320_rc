a82a37b7e871af333006df0feddbf305124245d8
Aidan <aks5238@rit.edu>
2018-06-26 18:09:16 -0400

    models: initial Store and Address

---
+++ src/main/java/com/rainforestcommerce/rcdb/models/Address.java
@@ -0,0 +1,23 @@
+package com.rainforestcommerce.rcdb.models;
+
+public class Address {
+
+    private String city;
+    private String state;
+    private String zipcode;
+    private String street;
+    private String number;
+
+    public Address(String number, String street, String city, String state, String zipcode){
+        this.number = number;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s %s %s, %s %s", number, street, city, state, zipcode);
+    }
+}
