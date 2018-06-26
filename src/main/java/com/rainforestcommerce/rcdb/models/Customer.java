be8703bc5e81642cedd036eeeb86762f2eebb400
Aidan <aks5238@rit.edu>
2018-06-26 16:10:26 -0400

    models: initial Customer model

---
+++ src/main/java/com/rainforestcommerce/rcdb/models/Customer.java
@@ -0,0 +1,66 @@
+package com.rainforestcommerce.rcdb.models;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class Customer {
+    public static SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+
+    private long accountNumber;
+    private String customerName;
+    private Date birthDate;
+    private boolean isMale;
+    private String phone;
+
+    public Customer(List<String> data) throws ParseException{
+        this.accountNumber = Long.parseLong(data.get(0));
+        this.customerName = data.get(1);
+        this.birthDate = sdf.parse(data.get(2));
+        this.isMale = "Male".equals(data.get(3));
+        //this.ethnicity = data.get(4);
+        this.phone = (data.get(5)).replaceAll("\\D+","");
+        //this.purchase_points = Integer.parseInt(data.get(6));
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long account_number) {
+        this.accountNumber = account_number;
+    }
+
+    public String getCustName() {
+        return customerName;
+    }
+
+    public void setCustName(String cust_name) {
+        this.customerName = cust_name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birth_date) {
+        this.birthDate = birth_date;
+    }
+
+    public boolean isIsMale() {
+        return isMale;
+    }
+
+    public void setIsMale(boolean is_male) {
+        this.isMale = is_male;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
