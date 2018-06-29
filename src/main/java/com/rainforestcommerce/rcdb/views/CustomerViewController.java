e5ac09f7a0fcb012546ef4173d047a2a53120696
Graham Home <grahamhome333@gmail.com>
2018-06-29 13:45:14 -0400

    Added logic to create table rows for customers & test code for this feature

--- src/main/java/com/rainforestcommerce/rcdb/views/CustomerViewController.java
+++ src/main/java/com/rainforestcommerce/rcdb/views/CustomerViewController.java
@@ -1,21 +1,52 @@
 package com.rainforestcommerce.rcdb.views;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.text.ParseException;
 import java.util.Arrays;
+import java.util.Date;
+
 
 import com.rainforestcommerce.rcdb.models.Customer;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 
 public class CustomerViewController {
 	
 	private static VBox view;
+	
+	@FXML
+	private TableView<Customer> customer_table;
+	
 	static {
 		try {
 			view = FXMLLoader.load(CustomerViewController.class.getResource("/CustomerView.fxml"));
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void initialize() {
+		TableColumn<Customer, String> nameColumn = (TableColumn<Customer, String>) customer_table.getColumns().get(0);
+		TableColumn<Customer, Date> dobColumn = (TableColumn<Customer, Date>) customer_table.getColumns().get(1);
+		TableColumn<Customer, Integer> pointsColumn = (TableColumn<Customer, Integer>) customer_table.getColumns().get(2);
+		
+		nameColumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("customerName"));
+		dobColumn.setCellValueFactory(new PropertyValueFactory<Customer, Date>("birthDate"));
+		
+		try {
+			ObservableList<Customer> customers = FXCollections.observableArrayList(
+				new Customer(Arrays.asList(new String[] {"1", "Graham", "08/03/1993", "true", "whatever", "1234567890"} )),
+				new Customer(Arrays.asList(new String[] {"2", "Abdul", "09/19/1996", "true", "whatever", "1234567890"} ))
+			);
+			customer_table.setItems(customers);
+		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 	}
@@ -26,7 +57,6 @@ public class CustomerViewController {
 		// add click handler to each row
 		
 		// TEST CODE
-		ArrayList<Customer> customers = new ArrayList<>();
 		return view;
 	}
 
