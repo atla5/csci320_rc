0af4aac8619913e7b026074111588d74d688b3c0
Graham Home <grahamhome333@gmail.com>
2018-06-29 14:02:07 -0400

    Bugfix

--- src/main/java/com/rainforestcommerce/rcdb/models/Customer.java
+++ src/main/java/com/rainforestcommerce/rcdb/models/Customer.java
@@ -28,11 +28,11 @@ public class Customer {
         return accountNumber;
     }
 
-    public String getCustName() {
+    public String getCustomerName() {
         return customerName;
     }
 
-    public void setCustName(String cust_name) {
+    public void setCustomerName(String cust_name) {
         this.customerName = cust_name;
     }
 
