a3beab3b751b3678a131092ffa5be23e6ebd7bac
Ethan Hughes <ethanol722@gmail.com>
2018-06-29 13:05:31 -0400

    Edited Customer.

--- src/main/java/com/rainforestcommerce/rcdb/controllers/CustomerProxy.java
+++ src/main/java/com/rainforestcommerce/rcdb/controllers/CustomerProxy.java
@@ -5,7 +5,7 @@ import com.rainforestcommerce.rcdb.models.Customer;
 import java.util.ArrayList;
 
 public class CustomerProxy {
-    
+
     public static ArrayList<Customer> getCustomers() {
         //Write Database Access code here
         return new ArrayList<Customer>();
