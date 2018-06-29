007cf7ac5cbc42c6bf1fad36f700fd4f0cfe6541
Ethan Hughes <ethanol722@gmail.com>
2018-06-29 14:46:43 -0400

    Added Connection Proxy as well as modifying stubs to connect to the database.

---
+++ src/main/java/com/rainforestcommerce/rcdb/controllers/ConnectionProxy.java
@@ -0,0 +1,13 @@
+package com.rainforestcommerce.rcdb.controllers;
+
+import org.h2.jdbcx.JdbcConnectionPool;
+
+public class ConnectionProxy {
+
+    public static JdbcConnectionPool cp = JdbcConnectionPool.create(
+            "jdbc:h2:~/test", "sa", "password");
+
+    public static void endConnection(){
+        cp.dispose();
+    }
+}
