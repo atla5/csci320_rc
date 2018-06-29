5ff669eebe1057ecd77270ac791bae6cf61d3922
Ethan Hughes <ethanol722@gmail.com>
2018-06-29 11:52:36 -0400

    Added getCustomers

--- src/main/java/com/rainforestcommerce/rcdb/controllers/CustomerProxy.java
+++ src/main/java/com/rainforestcommerce/rcdb/controllers/CustomerProxy.java
@@ -1,4 +1,7 @@
 package com.rainforestcommerce.rcdb.controllers;
 
 public class CustomerProxy {
+    public static ArrayList<Customer> getCustomers() {
+        return new ArrayList<Customer>();
+    }
 }
