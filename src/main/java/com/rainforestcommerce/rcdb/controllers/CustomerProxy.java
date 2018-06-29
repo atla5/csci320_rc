5cd513b7b6f9d9610828a699b25923a858a09d01
Ethan Hughes <ethanol722@gmail.com>
2018-06-29 12:21:38 -0400

    Added stubs for CustomerProxy

--- src/main/java/com/rainforestcommerce/rcdb/controllers/CustomerProxy.java
+++ src/main/java/com/rainforestcommerce/rcdb/controllers/CustomerProxy.java
@@ -1,7 +1,33 @@
 package com.rainforestcommerce.rcdb.controllers;
 
+import com.rainforestcommerce.rcdb.models.Customer;
+
+import com.rainforestcommerce.rcdb.models.Product;
+
+import com.rainforestcommerce.rcdb.models.ProductPurchase;
+
+import java.util.ArrayList;
+
 public class CustomerProxy {
     public static ArrayList<Customer> getCustomers() {
+        //Write Database Access code here
         return new ArrayList<Customer>();
+    }
+
+    public static ArrayList<Product> getProducts() {
+        //Write Database Access code here
+        return new ArrayList<Product>();
+    }
+
+    public static void purchaseProducts(ProductPurchase purchase){
+        //Add Product Purchases to database
+    }
+
+    public static boolean purchasable(ProductPurchase purchase){
+        return true;
+    }
+
+    public static ArrayList<Product> searchProducts(String search){
+        return new ArrayList<Product>();
     }
 }
