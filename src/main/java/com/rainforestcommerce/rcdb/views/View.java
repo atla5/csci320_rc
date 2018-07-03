8305952882b9b3b2230c0bc37e6d8a85ca195f27
Graham Home <grahamhome333@gmail.com>
2018-07-03 13:00:11 -0400

    Updated main class to launch view after successful DB connection, added documentation

--- src/main/java/com/rainforestcommerce/rcdb/views/View.java
+++ src/main/java/com/rainforestcommerce/rcdb/views/View.java
@@ -5,16 +5,24 @@ import com.rainforestcommerce.rcdb.views.ActivityManager.Activity;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * The primary view class which starts the view component of the application.
+ *
+ */
 public class View extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		ActivityManager.setStage(primaryStage);
-		ActivityManager.start(Activity.PRODUCTS);
+		ActivityManager.start(Activity.START_SCREEN);
 		primaryStage.show();
-		
 	}
     
+	/**
+	 * Method to launch the view independently of the rest of the application,
+	 * i.e. for testing purposes
+	 * @param args Not used
+	 */
 	public static void main(String[] args) {
 		launch();
 	}
