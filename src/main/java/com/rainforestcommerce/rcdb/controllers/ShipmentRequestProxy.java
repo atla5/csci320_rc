9140ebfe061ea2b54651bf3e3bbf5a73c059125b
Ethan <ethanol722@gmail.com>
2018-07-03 01:45:42 -0400

    Gave shipment request proxy an SQL statement to launch for getting products.

--- src/main/java/com/rainforestcommerce/rcdb/controllers/ShipmentRequestProxy.java
+++ src/main/java/com/rainforestcommerce/rcdb/controllers/ShipmentRequestProxy.java
@@ -13,9 +13,15 @@ import org.h2.jdbcx.JdbcConnectionPool;
 public class ShipmentRequestProxy {
 
     public static ArrayList<Product> getProductsForShipment(ShipmentRequest shipment){
-        String statement = "";
         Connection conn = ConnectionProxy.cp.getConnection();
-        conn.createStatement().execute(statement);
+        PreparedStatement statement = onnection.prepareStatment("SELECT data FROM Product WHERE ID = ?");
+        statement.setString(1, shipment.prodID);
+        ResultSet rs = conn.createStatement().executeQuery(statement);
+        ArrayList<Product> products;
+        products = rs.getArray("data");
+        //while(rs.next()){
+        //    products.add(new Product(rs.getString("data")));
+        //}
         conn.close();
         //Write Database Access code here
         return new ArrayList<Product>();
