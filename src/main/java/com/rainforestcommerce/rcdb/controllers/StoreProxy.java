ee369d50e378ea2e73ad545a43dfa921a51046fe
Ethan Hughes <ethanol722@gmail.com>
2018-07-03 13:49:12 -0400

    Added H2 and SQL Query code for store functions.

--- src/main/java/com/rainforestcommerce/rcdb/controllers/StoreProxy.java
+++ src/main/java/com/rainforestcommerce/rcdb/controllers/StoreProxy.java
@@ -40,51 +40,78 @@ public class StoreProxy {
 
     public static ArrayList<ProductPurchase> getPurchasesForStore(Store store){
         Connection conn = ConnectionProxy.cp.getConnection();
-        String statement"SELECT * FROM ProductPurchases";
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM ProductPurchases WHERE storeID = ?");
+        statement.setString(1, Long.toString(store.getStoreId()));
         ResultSet rs = conn.createStatement().executeQuery(statement);
-        ArrayList<Store> stores = null;
+        ArrayList<ProductPurchase> purchases = null;
         while(rs.next()){
-            stores.add(new Store(
-                    rs.getLong("storeId"),
-                    rs.getString("name"),
-                    rs.getDate("openingTime"),
-                    rs.getDate("closingTime"),
-                    rs.getString("city"),
-                    rs.getString("state"),
-                    rs.getString("street"),
-                    rs.getString("zip"),
-                    rs.getString("number")
+            purchases.add(new ProductPurchase(
+                    rs.getLong("purchaseId"),
+                    rs.getLong("productId"),
+                    rs.getInt("unit_price"),
+                    rs.getInt("quantity")
             ));
         }
         conn.close();
         //Write Database Access code here
-        return new ArrayList<ProductPurchase>();
+        return purchases;
     }
 
     public static ArrayList<ShipmentRequest> getShipmentsForStore(Store store){
-        String statement = "";
         Connection conn = ConnectionProxy.cp.getConnection();
-        conn.createStatement().execute(statement);
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM ShipmentRequest WHERE storeID = ?");
+        statement.setString(1, Long.toString(store.getStoreId()));
+        ResultSet rs = conn.createStatement().executeQuery(statement);
+        ArrayList<ShipmentRequest> shipments = null;
+        while(rs.next()){
+            shipments.add(new ShipmentRequest(
+                    rs.getLong("purchaseId"),
+                    rs.getLong("productId"),
+                    rs.getInt("unit_price"),
+                    rs.getInt("quantity")
+            ));
+        }
         conn.close();
         //Write Database Access code here
-        return new ArrayList<ShipmentRequest>();
+        return shipments;
     }
 
     public static ArrayList<Product> getProductsForStore(Store store){
-        String statement = "";
         Connection conn = ConnectionProxy.cp.getConnection();
-        conn.createStatement().execute(statement);
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM Products WHERE storeID = ?");
+        statement.setString(1, Long.toString(store.getStoreId()));
+        ResultSet rs = conn.createStatement().executeQuery(statement);
+        ArrayList<Product> products = null;
+        while(rs.next()){
+            products.add(new Product(
+                    rs.getLong("upcCode"),
+                    rs.getString("name"),
+                    rs.getInt("weight"),
+                    rs.getString("brand")
+            ));
+        }
         conn.close();
         //Write Database Access code here
-        return new ArrayList<Product>();
+        return products;
     }
 
     public static ArrayList<Product> searchProducts(Store store, String search){
-        String statement = "";
         Connection conn = ConnectionProxy.cp.getConnection();
-        conn.createStatement().execute(statement);
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM Products WHERE storeID = ? AND CHARINDEX(?, name) > 0");
+        statement.setString(1, Long.toString(store.getStoreId()));
+        statement.setString(2, search);
+        ResultSet rs = conn.createStatement().executeQuery(statement);
+        ArrayList<Product> products = null;
+        while(rs.next()){
+            products.add(new Product(
+                    rs.getLong("upcCode"),
+                    rs.getString("name"),
+                    rs.getInt("weight"),
+                    rs.getString("brand")
+            ));
+        }
         conn.close();
         //Write Database Access code here
-        return new ArrayList<Product>();
+        return products;
     }
 }
