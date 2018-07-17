222b6490c0b76af8ce53cda45ab2f5e2626a6305
Aidan <aks5238@rit.edu>
2018-07-17 09:29:53 -0400

    dataloader: additional helpers for other tables

--- src/main/java/com/rainforestcommerce/rcdb/DataLoader.java
+++ src/main/java/com/rainforestcommerce/rcdb/DataLoader.java
@@ -5,13 +5,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class DataLoader {
     private static String dataDirectory = "/Users/aidan/projects/school/csci-320/src/main/resources/sample_data";
     private static String GENERIC_INSERT_STATEMENT = "INSERT into %s VALUES %s;";
 
     public static void main(String[] args){
-        loadStores();
+        //loadProducts();
+        //loadCustomers();
+        //loadStores();
+        //loadInventory();
+        //loadPurchases(); // store and product purchases
         System.exit(0);
     }
 
@@ -27,6 +32,24 @@ public class DataLoader {
             values = String.format("(%s, '%s', %s, '%s')",
                                     upcCode, productName, weight, brand);
             System.out.println(String.format(GENERIC_INSERT_STATEMENT, "products", values));
+        }
+    }
+
+    private static void loadCustomers(){
+        List<String[]> csvData = readCsvIntoListOfStringArrays("Customer.csv");
+        String values;
+        for(String[] data : csvData){
+            String customerId = data[0];
+            String customerName = data[1];
+            String birthDate = data[2];
+            boolean isMale = data[3].equalsIgnoreCase("male");
+            String ethnicity = data[4];
+            String phone = data[5];
+            int purchasePoints = Integer.parseInt(data[6]);
+
+            values = String.format("(%s, '%s', '%s', %b, '%s', '%s', %d)",
+                    customerId, customerName, birthDate, isMale, ethnicity, phone, purchasePoints);
+            System.out.println(String.format(GENERIC_INSERT_STATEMENT, "customers", values));
         }
     }
 
@@ -48,6 +71,41 @@ public class DataLoader {
                     storeId, storeName, openingTime, closingTime, addrNum, addrStreet, addrCity, addrState, addrZip
             );
             System.out.println(String.format(GENERIC_INSERT_STATEMENT, "stores", values));
+        }
+    }
+
+    private static void loadInventory(){
+        List<String[]> csvData = readCsvIntoListOfStringArrays("Store_Inventory.csv");
+        String values;
+        Random random = new Random();
+        for(String[] data : csvData){
+            String storeId = "" + random.nextInt(1000);
+            String productId = "" + random.nextInt(150);;
+            float unitPrice = Integer.parseInt(data[2])/(100*1.0f);
+            int quantity = Integer.parseInt(data[3]);
+
+            values = String.format("(%s, %s, %s, %d)", storeId, productId, unitPrice, quantity);
+            System.out.println(String.format(GENERIC_INSERT_STATEMENT, "store_inventory", values));
+        }
+    }
+
+    private static void loadPurchases(){
+        List<String[]> csvData = readCsvIntoListOfStringArrays("Purchase.csv");
+        String values;
+        Random random = new Random();
+        for(String[] data : csvData){
+            String purchaseId = data[0];
+            String datePurchased = data[1];
+            String storeId = data[3];
+            int customerId = random.nextInt(150);
+            String productId = "" + random.nextInt(150);
+            boolean online = data[5].equalsIgnoreCase("true");
+            int quantity = random.nextInt(8);
+
+            values = String.format("(%s, %s, %s, '%s', %b)", purchaseId, storeId, customerId, datePurchased, online);
+            System.out.println(String.format(GENERIC_INSERT_STATEMENT, "store_purchases", values));
+            values = String.format("(%s, %s, %d)", purchaseId, productId, quantity);
+            System.out.println(String.format(GENERIC_INSERT_STATEMENT, "product_purchases", values));
         }
     }
 
