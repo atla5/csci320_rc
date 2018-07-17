a052324f673f900ef65e04c7aae14f2cb43f6e60
Ethan <ethanol722@gmail.com>
2018-07-17 16:20:06 -0400

    Altered Rcdb main application to start connection through ConnectionProxy, so as to allow it to handle all connections to the database.

--- src/main/java/com/rainforestcommerce/rcdb/RcdbApplication.java
+++ src/main/java/com/rainforestcommerce/rcdb/RcdbApplication.java
@@ -1,4 +1,5 @@
 package com.rainforestcommerce.rcdb;
+import com.rainforestcommerce.rcdb.controllers.ConnectionProxy;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,12 +15,12 @@ public class RcdbApplication {
 
 		RcdbApplication rcdb = new RcdbApplication();
 
-		try {
-			Class.forName("org.h2.Driver");
-			rcdb.connection = DriverManager.getConnection("jdbc:h2:~/test", "sa","");
-			System.out.println("connection established");
-			View.launch();
-		} catch(ClassNotFoundException cnf){
+		//try {
+		//Class.forName("org.h2.Driver");
+		ConnectionProxy.startConnection();
+		System.out.println("connection established");
+		View.launch();
+		/*} catch(ClassNotFoundException cnf){
 			System.err.println("Error with h2 configuration. 'org.h2.Driver' not found.");
 			cnf.printStackTrace();
 			System.exit(1);
@@ -27,16 +28,16 @@ public class RcdbApplication {
 			System.err.println("Error with sql connection.");
 			sql.printStackTrace();
 			System.exit(1);
-		}
+		}*/
 	}
 
-	public Connection getConnection() {
+	/*public Connection getConnection() {
 		return connection;
 	}
 
 	public void closeConnection() throws SQLException{
 		this.connection.close();
-	}
+	}*/
 
 
 }
