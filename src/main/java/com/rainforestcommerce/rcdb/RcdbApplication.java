0bacbd5d7fe02cc5972f34a049c6013070b7a48f
Aidan <aks5238@rit.edu>
2018-07-29 14:50:22 -0400

    config: RcdbApplication - System.*.println -> logger

--- src/main/java/com/rainforestcommerce/rcdb/RcdbApplication.java
+++ src/main/java/com/rainforestcommerce/rcdb/RcdbApplication.java
@@ -3,11 +3,14 @@ package com.rainforestcommerce.rcdb;
 import com.rainforestcommerce.rcdb.views.View;
 import com.rainforestcommerce.rcdb.controllers.ConnectionProxy;
 
+import java.util.logging.Logger;
+
 import static com.rainforestcommerce.rcdb.controllers.DataLoader.loadData;
 import static com.rainforestcommerce.rcdb.controllers.TableCreator.createTables;
 
 
 public class RcdbApplication extends View {
+	private static final Logger logger = Logger.getLogger( RcdbApplication.class.getName() );
 	public static String RESOURCES_DIRECTORY = "/src/main/resources";
 
 	private static final boolean CREATE_TABLES_ON_STARTUP = false;
@@ -17,19 +20,19 @@ public class RcdbApplication extends View {
 	public static void main(String[] args) {
 
 		ConnectionProxy.startConnection();
-		System.out.println("initial connection established");
+		logger.info("initial connection established");
 
 		if(CREATE_TABLES_ON_STARTUP){
 			boolean tableCreationSuccessful = createTables();
 			if(!tableCreationSuccessful){
-				System.err.println("ERROR creating tables. Exiting with error status...");
+				logger.severe("ERROR creating tables. Exiting with error status...");
 				System.exit(1);
 			}
 		}
 		if(RUN_LOADERS_ON_STARTUP) {
 			boolean loadDataSuccessful = loadData();
 			if(!loadDataSuccessful){
-				System.err.println("ERROR loading data. Exiting with error status...");
+				logger.severe("ERROR loading data. Exiting with error status...");
 			}
 		}
 	    
